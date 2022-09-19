e467ce63a972f316f279b213a640f6aa133c7b27
lucas <fernandes20lucas@gmail.com>
2022-09-19 16:06:04 -0300

    adiciona a tag em desenvolvimento de acordo com a condição

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+const { REACT_APP_ON_DEVELOPMENT } = process.env;
+
 function App() {
   return (
     <div className="App">
+      { REACT_APP_ON_DEVELOPMENT === 'true' && <h3>Em desenvolvimento</h3> }
       <StrangerThings />
     </div>
   );
